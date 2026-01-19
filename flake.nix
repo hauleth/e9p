77c22caf4c59db8f0dd985ebf1ce3edddd7c5eae
Łukasz Niemier <~@hauleth.dev>
2026-01-19 23:35:03 +0100

    Add `kamid` to packages in dev shell to test 9p implementation
    To test against existing implementation I wanted some existing tool that
    can be used to access 9p server. Kamid has `kamiftp` tool that is
    FTP-like client for 9p servers. It needs some patches to work on macOS
    so I add these changes in Nix derivation.
    
    Unfortunately this will not work for client tests, but it is way easier
    to find simple 9p server than it is to find simple 9p client for some
    reason.

--- flake.nix
+++ flake.nix
@@ -9,6 +9,10 @@
   inputs.flake-parts.url = "github:hercules-ci/flake-parts";
   inputs.devenv.url = "github:cachix/devenv";
   inputs.systems.url = "github:nix-systems/default";
+  inputs.kamid = {
+    url = "github:omar-polo/kamid";
+    flake = false;
+  };
 
   outputs = {flake-parts, ...} @ inputs:
     flake-parts.lib.mkFlake {inherit inputs;} {
@@ -18,11 +22,19 @@
 
       systems = import inputs.systems;
 
-      perSystem = {pkgs, ...}: {
+      perSystem = {pkgs, ...}: let
+        kamid = pkgs.callPackage ./nix/kamid { inherit inputs; };
+      in {
         formatter = pkgs.alejandra;
+
+        packages = {
+          inherit kamid;
+        };
 
         devenv.shells.default = {
           languages.erlang.enable = true;
+
+          packages = [ kamid ];
         };
       };
     };
---
+++ nix/kamid/default.nix
@@ -0,0 +1,33 @@
+# SPDX-FileCopyrightText: 2026 Łukasz Niemier <~@hauleth.dev>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+{
+  inputs,
+
+  kamid,
+  autoreconfHook,
+  pkg-config,
+  bison,
+  stdenv,
+
+  lib
+}:
+
+kamid.overrideAttrs (old: {
+  version = "git";
+
+  src = inputs.kamid;
+
+  nativeBuildInputs = old.nativeBuildInputs ++ [
+    autoreconfHook
+    pkg-config
+    bison
+  ];
+
+  patches = lib.optionals stdenv.isDarwin [ ./macos.patch ];
+
+  meta.package.platforms = old.meta.package.platforms ++ [
+    "aarch64-darwin"
+  ];
+})
